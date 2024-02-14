07eb30c3355c57ea3dddea238c7cc1b08ac93e36
Bernard Omuse <bernardomuse22@gmail.com>
2024-02-14 07:31:59 +0300

    Bandari Icons

---
+++ icon.php
@@ -0,0 +1,34 @@
+
+<?php
+$icons = [
+            ['building', '<i class="fa-sharp fa-regular fa-building-user"></i>'],
+            ['Phone Icon', '<font-awesome-icon icon="fa-sharp fa-light fa-phone" />'],            
+            ['facebook', '<i class="fa-brands fa-facebook"></i>">'],    
+            ['youtube', '<i class="fa-brands fa-youtube"></i>'],
+            ['instagram', '<i class="fa-brands fa-instagram"></i>'],    
+            ['linkedin', '<i class="fa-brands fa-linkedin"></i>'],
+            ['whatsApp', '<i class="fa-brands fa-whatsapp"></i>'],    
+            ['square-poll-vertical', '<i class="fa-thin fa-square-poll-vertical"></i>'],
+            ['image', '<i class="fa-light fa-image"></i>'],    
+            ['envelop dot', '<i class="fa-regular fa-envelope-dot"></i>'],
+            ['wallet', '<i class="fa-regular fa-wallet"></i>'],    
+            ['wifi', '<i class="fa-regular fa-wifi"></i>'],
+            ['circle-check', '<i class="fa-regular fa-circle-check"></i>'],    
+            ['magnifying-glass-arrow-rotate', '<i class="fa-regular fa-magnifying-glass-arrows-rotate"></i>'],
+            ['city', '<i class="fa-regular fa-city"></i>'],    
+            ['money-bill', '<i class="fa-light fa-money-bill"></i>'],
+            ['envelop-circle-check', '<i class="fa-regular fa-envelope-circle-check"></i>'],    
+            ['wallet', '<i class="fa-regular fa-wallet"></i>'],
+            ['user', '<i class="fa-regular fa-user"></i>'],    
+            ['award', '<i class="fa-regular fa-award"></i>'],
+            ['calendar-days', '<i class="fa-regular fa-calendar-days"></i>'],    
+            ['handshake-handle', '<i class="fa-regular fa-handshake-angle">'],
+            ['angle-right', '<i class="fa-regular fa-angle-right"></i>'],    
+            ['angle-down', '<i class="fa-regular fa-angle-down"></i>'],
+            ['upload', '<i class="fa-regular fa-upload"></i>'],    
+            ['star', '<i class="fa-solid fa-star"></i>'],
+              
+            ['Awards Icon', 'fas fa-award'],
+            ['Phone Icon', 'fas fa-phone'],    
+            
+        ];
