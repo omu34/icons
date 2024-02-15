9e91c72442df10f8f4cfd6f09973eafec0af7d20
Bernard Omuse <79845295+omu34@users.noreply.github.com>
2024-02-15 12:11:18 +0300

    Update icon.php
    updated icons

--- icon.php
+++ icon.php
@@ -1,58 +1,34 @@
 
 <?php
 $icons = [
-    // HOME PAGE
-    // Top navigation
-    ['search button', 'fas fa-magnifying-glass-arrows-rotate'],
-    ['facebook', 'fas fa-facebook">'],
-    ['youtube', 'fas fa-youtube'],
-    ['instagram', 'fas fa-instagram'],
-    ['linkedin', 'fas fa-linkedin'],
-    ['whatsApp', 'fas fa-whatsapp'],
-    ['twitter', 'fas fa-square-x-twitter'],
-    // Product and Services Icons
-    ['web portal', 'fas fa-wifi'],
-    ['mobile banking', "fas fa-mobile-button"],
-    ['other services', 'fas fa-credit-card'],
-    ['loans', 'fas fa-chart-line'],
-    ['share capital', 'fas fa-bolt'],
-    ['savings and deposit', "fas fa-door-open"],
-    // Need Help Icons
-    ['support Hotline', "fas fa-phone"],
-    ['email@email.com', 'fas fa-envelope-dot'],
-    ['support button right arrow', 'fas fa-chevron-right'],
-    ['plus icon', 'fas fa-plus'],
-    // Footer Nav Icons
-    ['0724 002 525', 'fas fa-phone'],
-    ['info@bandari.co.ke', 'fas fa-envelope-circle-check'],
-    ['monday-friday 9am-16pm image', 'fas fa-city'],
-    ['bandari Sacco Building', 'fas fa-building-user'],
-    // Who we are
-    ['circular tick', 'fas fa-circle-check'],
-
-    // OUR RESOURCES PAGE
-    // downloads
-    ['downloads', 'fas fa-file-arrow-down'],
-    ['careers', 'fas fa-user'],
-
-    // ABOUT US PAGE
-    // Our Awards
-    ['award', 'fas fa-award'],
-    // our services
-    ['show more button', 'fas  fa-chevron-down'],
-    ['get in touch button', 'fas  fa-chevron-right'],
-
-    // OUR DOWNLOADS PAGE
-    // forms
-    ['forms icon', 'fas  fa-wpforms'],
-
-
-    // HELP AND SUPPORT PAGE
-    // membership
-    ['membership', 'fas fa-message'],
-
-    // SUBMIT TICKET PAGE
-    // Create New ticket
-    ['select and upload', 'fas fa-cloud-arrow-up'],
+     ['Search Button Icon', 'fas fa-magnifying-glass-arrows-rotate'],
+    ['Facebook Icon', 'fas fa-facebook">'],
+    ['Youtube Icon', 'fas fa-youtube'],
+    ['Instagram Icon', 'fas fa-instagram'],
+    ['Linkedin Icon', 'fas fa-linkedin'],
+    ['Whatsapp Icon', 'fas fa-whatsapp'],
+    ['Twitter Icon', 'fas fa-square-x-twitter'],        
+    ['Wifi Icon', 'fas fa-wifi'],
+    ['Mobile Icon', "fas fa-mobile-button"],
+    ['Credit Card Icon', 'fas fa-credit-card'],
+    ['Chart-line Icon', 'fas fa-chart-line'],
+    ['Bolt Icon', 'fas fa-bolt'],
+    ['Door Open Icon', "fas fa-door-open"],    
+    ['Phone Icon', "fas fa-phone"],
+    ['Email Icon', 'fas fa-envelope-dot'],
+    ['Right Arrow Icon', 'fas fa-chevron-right'],
+    ['Plus Icon', 'fas fa-plus'],    
+    ['Email Icon', 'fas fa-envelope-circle-check'],
+    ['City Icon', 'fas fa-city'],
+    ['Home Icon', 'fas fa-building-user'],    
+    ['Circle With A Tick Icon', 'fas fa-circle-check'],    
+    ['Downloads Icon', 'fas fa-file-arrow-down'],
+    ['User Icon', 'fas fa-user'],   
+    ['Awards Icon', 'fas fa-award'],    
+    ['Down Arrow Icon', 'fas  fa-chevron-down'],
+    ['Right Arrow Icon', 'fas  fa-chevron-right'],    
+    ['Form Icon', 'fas  fa-wpforms'],    
+    ['Message Icon', 'fas fa-message'],    
+    ['Upload Icon', 'fas fa-cloud-arrow-up'],
 
 ];
